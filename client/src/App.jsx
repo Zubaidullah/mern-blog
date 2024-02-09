4eff008b222e06934a3d263adf39d69a14732b26
Zubaidullah <26290579+Zubaidullah@users.noreply.github.com>
2024-02-09 16:41:47 +0000

    import the routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,24 @@
-import { BrowserRouter as Router, Route, Switch, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import React from 'react'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
+import About from './pages/About'
+import Projects from './pages/Projects'
+
 
 export default function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home/>} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/projects" element={<Projects/>} />
         </Routes>
       </BrowserRouter>
     </div>
