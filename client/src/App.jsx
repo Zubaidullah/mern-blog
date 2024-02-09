2c0dc5f75db6d91382e16ccaf80ba7cf425ac55b
Zubaidullah <26290579+Zubaidullah@users.noreply.github.com>
2024-02-09 16:33:38 +0000

    create all the routes and pages.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter as Router, Route, Switch, BrowserRouter } from 'react-router-dom'
 import React from 'react'
 
 export default function App() {
   return (
     <div>
-      <h1 className='text-6xl text-red-600'>Hello World</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
